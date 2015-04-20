60d99344f3371693b16ade0d37acf9620697d0e3
Jonathan Lin & Brian Kropff <student@epicodus.com>
2015-04-20 10:59:33 -0700

    Add deleteStudent function

--- js/StudentController.js
+++ js/StudentController.js
@@ -1,9 +1,11 @@
 studentRoster.controller('StudentCtrl', function StudentCtrl($scope) {
-    $scope.students = [
-        { name: "Sam Schmidt" },
-        { name: "Jessica Martin" },
-        { name: "Sandy Smith" },
-        { name: "Ryan Samuels"},
-        { name: "Brentwood Davis" }
-    ]
+    $scope.students = [];
+    $scope.addStudent = function() {
+        $scope.students.push({ name: $scope.studentName });
+        $scope.studentName = null;
+    }
+    $scope.deleteStudent = function(student) {
+        var index = $scope.students.indexOf(student);
+        $scope.students.splice(index, 1)
+    }
 });
