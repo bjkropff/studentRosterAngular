e16186d3d73a7bb606a25684ba5aef33b7fcfe45
Jonathan Lin & Brian Kropff <student@epicodus.com>
2015-04-20 09:57:58 -0700

    Add Angular functional that loops through a list

---
+++ js/StudentController.js
@@ -0,0 +1,9 @@
+studentRoster.controller('StudentCtrl', function StudentCtrl($scope) {
+    $scope.students = [
+        { name: "Sam Schmidt" },
+        { name: "Jessica Martin" },
+        { name: "Sandy Smith" },
+        { name: "Ryan Samuels"},
+        { name: "Brentwood Davis" }
+    ]
+});
