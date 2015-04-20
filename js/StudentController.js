cd41f79ae2dcf3c941e341a9489c2991fcceb7aa
Jonathan Lin & Brian Kropff <student@epicodus.com>
2015-04-20 11:43:24 -0700

    Refactor and clean up html code

--- js/StudentController.js
+++ js/StudentController.js
@@ -1,8 +1,9 @@
 studentRoster.controller('StudentCtrl', function StudentCtrl($scope) {
     $scope.students = [];
     $scope.addStudent = function() {
-        $scope.students.push({ name: $scope.studentName });
+        $scope.students.push({ name: $scope.studentName , age: $scope.studentAge });
         $scope.studentName = null;
+        $scope.studentAge = null;
     }
     $scope.deleteStudent = function(student) {
         var index = $scope.students.indexOf(student);
